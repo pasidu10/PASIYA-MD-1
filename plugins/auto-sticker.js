dd8005db54ae9c3e9295427436bbfe3316cf4d20
PASIYAv12 <pasidusampath280@gmail.com>
2025-07-12 11:33:09 +0530

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -6,28 +6,56 @@ const { ven } = require('../pasiya');
 
 ven({
   on: "body"
-},
-async (conn, mek, m, { from, body }) => {
-    const filePath = path.join(__dirname, '../all/autosticker.json');
+},    
+async (robin, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../my_data/autovoice.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            if (config.AUTO_STICKER === 'true') {
-                const stickerPath = path.join(__dirname, '../all/autosticker', data[text]);
-
-                if (fs.existsSync(stickerPath)) {
-                    const stickerBuffer = fs.readFileSync(stickerPath);
-
-                    await conn.sendMessage(from, {
-                        sticker: stickerBuffer,
-                        packname: '𝕽𝖆𝖛𝖊𝖓',
-                        author: '𝕽𝖆𝖛𝖊𝖓'
-                    }, { quoted: mek });
-                } else {
-                    console.warn(`Sticker not found: ${stickerPath}`);
-                }
+            
+            if (config.AUTO_VOICE === 'true') {
+                //if (isOwner) return;        
+                await robin.sendPresenceUpdate('recording', from);
+                await robin.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
             }
         }
-    }
+    }                
 });
+
+//auto sticker 
+ven({
+  on: "body"
+},    
+async (robin, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../my_data/autosticker.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            
+            if (config.AUTO_STICKER === 'true') {
+                //if (isOwner) return;        
+                await robin.sendMessage(from,{sticker: { url : data[text]},package: 'S_I_H_I_L_E_L'},{ quoted: mek })   
+            
+            }
+        }
+    }                
+});
+
+//auto reply 
+ven({
+  on: "body"
+},    
+async (robin, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../my_data/autoreply.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            
+            if (config.AUTO_REPLY === 'true') {
+                //if (isOwner) return;        
+                await m.reply(data[text])
+            
+            }
+        }
+    }                
+});                  

3fd3f273ba473f2557a5023c304c37b7afc7e97f
PASIYAv12 <pasidusampath280@gmail.com>
2025-07-14 18:40:55 +0530

    Create move.js

---
+++ move.js
@@ -0,0 +1,171 @@
+{
+  "data": {
+    "author": "PASIYA-MD",
+    "data": {
+      "mainDetails": {
+        "maintitle": "Captain America: Brave New World (2025) Sinhala Subtitles | සිංහල උපසිරසි සමඟ - SinhalaSub",
+        "imageUrl": "https://sinhalasub.lk/wp-content/uploads/2025/03/pzIddUEMWhWzfvLI3TwxUG2wGoi-200x300.jpg",
+        "rating": {
+          "value": "6.6",
+          "count": "5"
+        },
+        "genres": [
+          "2025 Relea",
+          "Action",
+          "Science Fiction",
+          "Thriller"
+        ],
+        "dateCreated": "Feb. 12, 2025",
+        "country": "USA",
+        "runtime": "119 Min."
+      },
+      "moviedata": {
+        "title": "Captain America: Brave New World (2025) Sinhala Subtitles | සිංහල උපසිරසි සමඟ - SinhalaSub",
+        "description": "මෙම චිත්‍රපටයේ  රංගනයෙන් දායකවන නළු නිලියන්ගේ  වෙනත් චිත්‍රපට නැරඹීමට පහල ඔවුන්ේ පින්තුර Click කර එම List එකට පිවිසේන",
+        "imdbRating": "6",
+        "imdbvotesCount": "68,855",
+        "imageUrls": [
+          "https://sinhalasub.lk/wp-content/uploads/2023/08/subnet-temp-text.gif",
+          "https://sinhalasub.lk/wp-content/uploads/2025/03/Captain-America-Brave-New-World-2025.jpg\n",
+          "https://image.tmdb.org/t/p/w300/ywe9S1cOyIhR5yWzK7511NuQ2YX.jpg\n",
+          "https://image.tmdb.org/t/p/w300/qfAfE5auxsuxhxPpnETRAyTP5ff.jpg\n",
+          "https://image.tmdb.org/t/p/w300/8eifdha9GQeZAkexgtD45546XKx.jpg\n",
+          "https://image.tmdb.org/t/p/w300/3nPDlMUEOLsJcdzSKsR6sKYBDEI.jpg\n",
+          "https://image.tmdb.org/t/p/w300/rw9fSaQ4jkm4tDwn5qIXWBRmNEs.jpg\n",
+          "https://image.tmdb.org/t/p/w300/eJLTpgUAFkx165LuUoQqQGyN5Wp.jpg\n",
+          "https://image.tmdb.org/t/p/w300/dABY79DsCyXjFnNGKl5ImOxA6O0.jpg\n",
+          "https://image.tmdb.org/t/p/w300/sRF5PBGGhEwxod7QzBh8grM7TrQ.jpg\n",
+          "https://image.tmdb.org/t/p/w300/8OBEXLojBqa3e8DC1hJxHnfRcWQ.jpg\n"
+        ],
+        "director": "Julius Onah",
+        "tags": [
+          "English"
+        ],
+        "subtitlesBy": "අංජුල උමේෂ්",
+        "cast": [
+          {
+            "name": "Julius Onah",
+            "character": "Director",
+            "photo": "https://image.tmdb.org/t/p/w92/1dDPLY788B9qwQ7qdJazhcApybJ.jpg"
+          },
+          {
+            "name": "Anthony Mackie",
+            "character": "Sam Wilson / Captain America",
+            "photo": "https://image.tmdb.org/t/p/w92/eZSIDrtTzhvabyjrmIITQLsjx8h.jpg"
+          },
+          {
+            "name": "Harrison Ford",
+            "character": "President Thaddeus Ross",
+            "photo": "https://image.tmdb.org/t/p/w92/zVnHagUvXkR2StdOtquEwsiwSVt.jpg"
+          },
+          {
+            "name": "Danny Ramirez",
+            "character": "Joaquin Torres",
+            "photo": "https://image.tmdb.org/t/p/w92/1CMMfxwMYOme8AOrl4kZS12nJpM.jpg"
+          },
+          {
+            "name": "Shira Haas",
+            "character": "Ruth Bat-Seraph",
+            "photo": "https://image.tmdb.org/t/p/w92/5mfd9yKnbo2ZjU42vmqVMgvTJ8r.jpg"
+          },
+          {
+            "name": "Tim Blake Nelson",
+            "character": "Samuel Sterns",
+            "photo": "https://image.tmdb.org/t/p/w92/rWuTGiAMaaHIJ30eRkQS23LbRSW.jpg"
+          },
+          {
+            "name": "Carl Lumbly",
+            "character": "Isaiah Bradley",
+            "photo": "https://image.tmdb.org/t/p/w92/ew1URcenWNl1Uclsz9ADiRb0uBD.jpg"
+          },
+          {
+            "name": "Giancarlo Esposito",
+            "character": "Sidewinder",
+            "photo": "https://image.tmdb.org/t/p/w92/rcXnr82TwDzU4ZGdBeNXfG0ZQnZ.jpg"
+          },
+          {
+            "name": "Liv Tyler",
+            "character": "Betty Ross",
+            "photo": "https://image.tmdb.org/t/p/w92/eQnuADVICaY40nl2ZseYvfkGQCc.jpg"
+          },
+          {
+            "name": "Xosha Roquemore",
+            "character": "Leila Taylor",
+            "photo": "https://image.tmdb.org/t/p/w92/k596seeX26xKN8bZ3Uir9zFJ2gS.jpg"
+          },
+          {
+            "name": "Jóhannes Haukur Jóhannesson",
+            "character": "Copperhead",
+            "photo": "https://image.tmdb.org/t/p/w92/oqZftP0WS1rD5NFpR7vLp6JU52I.jpg"
+          }
+        ]
+      },
+      "dllinks": {
+        "directDownloadLinks": [
+          {
+            "id": "download",
+            "quality": "FHD 1080pFHD 1080p",
+            "size": "2.32 GB",
+            "link": "https://sinhalasub.lk/links/uagau8fl7t/"
+          },
+          {
+            "id": "download",
+            "quality": "HD 720pHD 720p",
+            "size": "1.19 GB",
+            "link": "https://sinhalasub.lk/links/ik8h7ll6i7/"
+          },
+          {
+            "id": "download",
+            "quality": "SD 480pSD 480p",
+            "size": "650 MB",
+            "link": "https://sinhalasub.lk/links/zbg7nwus87/"
+          },
+          {
+            "id": "download-03",
+            "quality": "FHD 1080pFHD 1080p",
+            "size": "2.32 GB",
+            "link": "https://sinhalasub.lk/links/vv514ootcs/"
+          },
+          {
+            "id": "download-03",
+            "quality": "HD 720pHD 720p",
+            "size": "1.19 GB",
+            "link": "https://sinhalasub.lk/links/biensutmun/"
+          },
+          {
+            "id": "download-03",
+            "quality": "SD 480pSD 480p",
+            "size": "650 MB",
+            "link": "https://sinhalasub.lk/links/aerezjiloi/"
+          },
+          {
+            "id": "සිංහල උපසිරසි",
+            "quality": "SubtitlesSubtitles",
+            "size": "----",
+            "link": "https://sinhalasub.lk/links/jhxhzkn9ea/"
+          }
+        ],
+        "telegramDownloadLinks": [
+          {
+            "id": "Telagram",
+            "quality": "FHD 1080pFHD 1080p",
+            "size": "2.32 GB",
+            "link": "https://sinhalasub.lk/links/df2hkql8kl/"
+          },
+          {
+            "id": "Telagram",
+            "quality": "HD 720pHD 720p",
+            "size": "1.19 GB",
+            "link": "https://sinhalasub.lk/links/w1bdofskte/"
+          },
+          {
+            "id": "Telagram",
+            "quality": "SD 480pSD 480p",
+            "size": "650 MB",
+            "link": "https://sinhalasub.lk/links/o6ooovmhqn/"
+          }
+        ]
+      }
+    }
+  }
+}

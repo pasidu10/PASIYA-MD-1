021070d42c3cc471783a7fa07fe191ef3496e4f6
PASIYAv12 <pasidusampath280@gmail.com>
2025-08-25 19:05:07 +0530

    Update honder.js

--- honder.js
+++ honder.js
@@ -6,3 +6,6 @@ if (m.message?.buttonsResponseMessage) {
 if (command === "forex" || command === "fx") {
    require("./commands/forex").handle(sock, m, args)
 }
+if (command === "binance" || command === "crypto" || command === "btc" || command === "eth" || command === "bnb" || command === "xrp") {
+   require("./commands/binance").handle(sock, m, args, command)
+}

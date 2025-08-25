bdf4efc0394c90d6eb54b6a1b9c65e4a1418940d
PASIYAv12 <pasidusampath280@gmail.com>
2025-08-25 18:55:50 +0530

    Update honder.js

--- honder.js
+++ honder.js
@@ -3,3 +3,6 @@ if (m.message?.buttonsResponseMessage) {
     const buttonId = m.message.buttonsResponseMessage.selectedButtonId
     require("./commands/settings").handleButton(sock, m, buttonId)
 }
+if (command === "forex" || command === "fx") {
+   require("./commands/forex").handle(sock, m, args)
+}
